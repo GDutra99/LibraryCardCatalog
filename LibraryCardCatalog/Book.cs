e5517f118e808b9bc126b06f4fa26e5e2aef073a
Clayton Halaska <claytonhalaska@gmail.com>
2016-08-24 15:46:21 -0500

    Added Genre

--- LibraryCardCatalog/Book.cs
+++ LibraryCardCatalog/Book.cs
@@ -12,6 +12,28 @@ namespace LibraryCardCatalog
         public string Author { get; set; }
         public int YearPublished { get; set; }
         public string Type { get; set; }
+        public Genre Genre { get; set; }
+
+    }
+    public enum Genre
+    {
+        ScienceFiction,
+        Fiction,
+        Satire,
+        Drama,
+        ActionAdventure,
+        Romance,
+        Mystery,
+        Horror,
+        SelfHelp,
+        Health,
+        Children,
+        Science,
+        History,
+        Poetry,
+        Comics,
+        Art,
+        Cookbooks
 
     }
 }
