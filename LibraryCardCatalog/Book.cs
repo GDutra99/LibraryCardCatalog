9571991c126155fe4c6b5b4584ebc2771414e84d
Gabriel Dutra <GDutra99@iCloud.com>
2016-08-24 15:33:56 -0500

    Library

---
+++ LibraryCardCatalog/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCardCatalog
+{
+     public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int YearPublished { get; set; }
+        public string Type { get; set; }
+
+    }
+}
