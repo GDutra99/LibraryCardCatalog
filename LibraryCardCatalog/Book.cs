d1f8320ad7710781d90cefe6d1f7c19620922b55
Gabriel Dutra <GDutra99@iCloud.com>
2016-08-24 15:56:27 -0500

    Changed book.

--- LibraryCardCatalog/Book.cs
+++ LibraryCardCatalog/Book.cs
@@ -11,7 +11,6 @@ namespace LibraryCardCatalog
         public string Title { get; set; }
         public string Author { get; set; }
         public int YearPublished { get; set; }
-        public string Type { get; set; }
         public Genre Genre { get; set; }
 
     }
